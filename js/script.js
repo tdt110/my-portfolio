2ed0e7e3837e3af7d65f92c94f6dffe655fa4c6f
tdt110 <tranduytan882@gmail.com>
2026-07-17 09:32:43 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio của Trần Duy Tân đã load xong!');
+
+    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
+});
